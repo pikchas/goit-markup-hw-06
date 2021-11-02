b21cc32bc604b5d31d74c6c95f392691aaf9e6d9
Alexander Pikurin <pikchas95@gmail.com>
2021-11-02 13:30:03 +0200

    верстка форм

--- js/modal.js
+++ js/modal.js
@@ -12,3 +12,14 @@
     refs.modal.classList.toggle('is-hidden');
   }
 })();
+(() => {
+  document.querySelector('.js-speaker-form')
+    .addEventListener('submit', e => {
+    e.preventDefault();
+
+    new FormData(e.currentTarget).forEach((value, name) =>
+      console.log(`${name}: ${value}`),
+      );
+      e.currentTarget.removeEventListener();
+  });
+})();
